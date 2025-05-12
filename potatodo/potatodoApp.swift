5c736c8d58913bfde1630414062948bbfe64f3df
Jeremy <jeremymcpaton@gmail.com>
2025-05-12 13:53:08 +1000

    Initial Commit

---
+++ potatodo/potatodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  potatodoApp.swift
+//  potatodo
+//
+//  Created by Jeremy Paton on 12/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct potatodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ potatodoTests/potatodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  potatodoTests.swift
+//  potatodoTests
+//
+//  Created by Jeremy Paton on 12/5/2025.
+//
+
+import Testing
+@testable import potatodo
+
+struct potatodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
